0a4ced7db26b32e3a97c4f866d3378aa3ac677db
Megios <ad.messaoudene@gmail.com>
2022-06-29 14:16:19 +0200

    page contact

--- js/script.js
+++ js/script.js
@@ -23,3 +23,14 @@ p3.addEventListener("mouseenter", function(event){
 p3.addEventListener("mouseleave", function(event){
   par3.classList.add('cacher')
 })
+
+const modalContainer= document.querySelector(".modal-container");
+const modalTriggers= document.querySelectorAll(".modal-trigger");
+console.log(modalTriggers)
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+function toggleModal(){
+  modalContainer.classList.toggle("active");
+  console.log(modalContainer)
+}
